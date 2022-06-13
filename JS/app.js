d6ed3da3b057c649655935e7a4fbd611ff7e8106
SumiHasan086 <94843983+SumiHasan086@users.noreply.github.com>
2022-06-13 20:04:13 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById("btn")
+var light = document.getElementById("light")
+
+function toggleBtn() {
+    btn.classList.toggle("active")
+    light.classList.toggle("on")
+}
+
+btn.addEventListener("click", () => {
+    toggleBtn()
+})
